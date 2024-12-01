1189b0650777426f08ab1e0edfb39907eb1ea9df
Hope-Kerubo <kerubohopes@gmail.com>
2024-12-01 23:03:20 +0300

    Resolved merge conflicts

---
+++ FemBloomApp/urls.py
@@ -0,0 +1,18 @@
+
+from django.contrib import admin
+from django.urls import path
+from FemBloomApp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('modules', views.modules, name='modules'),
+    path('about', views.about, name='about'),
+    path('contact', views.contact, name='contact'),
+    path('hf', views.hf, name='hf'),
+    path('blog', views.blog, name='blog'),
+    path('feature', views.feature, name='feature'),
+    path('testimonial', views.testimonial, name='testimonial'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+]
--- FemBloomApp/views.py
+++ FemBloomApp/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def modules(request):
+    return render(request, 'modules.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def feature(request):
+    return render(request, 'feature.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def testimonial(request):
+    return render(request, 'testimonial.html')
+
+def hf(request):
+    return render(request, 'hf.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def signin(request):
+    return render(request, 'signin.html')
