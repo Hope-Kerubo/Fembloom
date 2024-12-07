706cdd638f66f59f3f63c29c536216c350c03d78
Hope-Kerubo <kerubohopes@gmail.com>
2024-12-07 09:34:46 +0300

    update

--- FemBloomApp/admin.py
+++ FemBloomApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from FemBloomApp.models import User
+from FemBloomApp.models import Module
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Module)
--- FemBloomApp/models.py
+++ FemBloomApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    Fullname = models.CharField(max_length=100)
+    EmailAddress = models.EmailField(max_length=100)
+    Password = models.CharField(max_length=100)
+    ConfirmPassword = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.Fullname
+
+class Module(models.Model):
+    module_name = models.CharField(max_length=100)
+    description = models.TextField()
+    content = models.TextField()
+    def __str__(self):
+        return self.module_name
+
+
